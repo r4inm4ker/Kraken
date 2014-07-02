8c328ebc7a9cbba89a170a6aa2616078c20a1cd1
Eric Thivierge <ethivierge@gmail.com>
2014-07-01 22:50:21 -0400

    Updates sample Softimage arm rig script.

--- plugins/si_plugin/kraken_siArmBuild.py
+++ plugins/si_plugin/kraken_siArmBuild.py
@@ -3,8 +3,10 @@ from kraken.tests import arm_rig
 
 from kraken.tests.arm_rig import ArmRig
 
+from kraken.plugins.si_plugin import builder
+reload(builder)
+
 from kraken.plugins.si_plugin.builder import SIBuilder
-reload(si_builder)
 
 myArm = ArmRig("myArm")
 
