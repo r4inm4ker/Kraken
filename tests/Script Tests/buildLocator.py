1fc4808d5a7b0097dc892d1ab07648096ee68919
julian <julian@passion-pictures.com>
2014-09-30 15:10:16 +0100

    setExplicitNaming set to True

--- tests/Script Tests/buildLocator.py
+++ tests/Script Tests/buildLocator.py
@@ -1,7 +1,10 @@
 from kraken import plugins
 from kraken.core.objects.locator import Locator
 
-myLoc = Locator("myLocator")
-
 builder = plugins.getBuilder()
+
+config = builder.getConfig()
+config.setExplicitNaming(True)
+
+myLoc = Locator("myLocator")
 builder.build(myLoc)
