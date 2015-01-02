81cac9eb4dd0d11f2887a89be0c2379875be03c1
phtaylor <phil.taylor@fabricengine.com>
2015-01-02 15:28:12 -0500

    - Added support for RotationOrder to Euler value.
    - Implemented the remainder of the methods.

--- core/maths/euler.py
+++ core/maths/euler.py
@@ -7,7 +7,31 @@ Euler -- Euler rotation.
 import math
 from kraken.core.objects.kraken_system import KrakenSystem as KS
 from math_object import MathObject
+from mat33 import Mat33
 
+rotationOrderStrToIntMapping = {  
+    'xyz': 0,
+    'XYZ': 0,
+    'yzx': 1,
+    'YZX': 1,
+    'zxy': 2,
+    'ZXY': 2,
+    'xzy': 3,
+    'XZY': 3,
+    'zyx': 4,
+    'ZYX': 4,
+    'yxz': 5,
+    'YXZ': 5
+}
+
+rotationOrderIntToStrMapping = [
+    'XYZ',
+    'YZX',
+    'ZXY',
+    'XZY',
+    'ZYX',
+    'YXZ'
+]
 
 class Euler(MathObject):
     """Euler rotation object."""
@@ -18,27 +42,36 @@ class Euler(MathObject):
         super(Euler, self).__init__()
 
         if x is not None and not isinstance(x, (int, float)):
-            raise TypeError("Euler: Invalid type for 'x' argument. Must be a int or float.")
+            raise TypeError("Euler: Invalid type for 'x' argument. Must be an int or float.")
 
         if y is not None and not isinstance(y, (int, float)):
-            raise TypeError("Euler: Invalid type for 'y' argument. Must be a int or float.")
+            raise TypeError("Euler: Invalid type for 'y' argument. Must be an int or float.")
 
         if z is not None and not isinstance(z, (int, float)):
-            raise TypeError("Euler: Invalid type for 'z' argument. Must be a int or float.")
+            raise TypeError("Euler: Invalid type for 'z' argument. Must be an int or float.")
 
-        # if ro is not None and not isinstance(ro, (int, float)):
-        #     raise TypeError("Euler: Invalid type for 'ro' argument. Must be a int or float.")
+        if ro is not None and not isinstance(ro, (int)):
+            if isinstance(ro, basestring):
+                ro = rotationOrderStrToIntMapping[ro]
+            else:
+                raise TypeError("Euler: Invalid type for 'ro' argument. Must be an int or a string.")
 
-        client = KS.getInstance().getCoreClient()
-        self.rtval = client.RT.types.Euler()
+        if x is not None and self.getTypeName(x) == 'Euler':
+            self.rtval = x
+        else:
+            self.rtval = KS.inst().rtVal('Euler')
+            if x is not None and y is not None and z is not None:
+                if ro is not None:
+                    self.set(x=x, y=y, z=z, ro=ro)
+                else:
+                    self.set(x=x, y=y, z=z)
 
-        # self.set(x, y, z, ro)
 
 
     def __str__(self):
         """String representation of Euler object."""
 
-        return "Euler(" + str(self.x) + "," + str(self.y) + "," + str(self.z) + "," + " ro: " + str(self.ro) + ")"
+        return "Euler(x=" + str(self.x) + ", y=" + str(self.y) + ", z=" + str(self.z) + ", ro= '" + str(self.ro) + "')"
 
 
     @property
@@ -143,7 +176,7 @@ class Euler(MathObject):
 
         """
 
-        return self.rtval.ro
+        return rotationOrderIntToStrMapping[self.rtval.ro.order]
 
 
     @ro.setter
@@ -157,20 +190,68 @@ class Euler(MathObject):
         True if successful.
 
         """
-
-        self.rtval.ro = KS.inst().rtVal('RotationOrder', value)
+        self.rtval.ro.order = KS.inst().rtVal('Integer', value)
 
 
     # Setter from scalar components
-    def set(self, x, y, z):
+    def set(self, x, y, z, ro=None):
         """Doc String.
 
         Arguments:
-        Arguments -- Type, information.
+            x, y, z -- Scalar, the angles in radians to set the eulaer angles to.
+            ro -- Integer, the rotation order to use in the euler angles.
 
         Return:
         True if successful.
 
         """
+        print "set(self, x, y, z, ro=None):" + str(ro)
+        self.rtval.set('', KS.inst().rtVal('Scalar', x), KS.inst().rtVal('Scalar', y), KS.inst().rtVal('Scalar', z))
+        if ro is not None:
+            if isinstance(ro, basestring):
+                ro = rotationOrderStrToIntMapping[ro]
+            self.rtval.ro.order = KS.inst().rtVal('Integer', ro)
 
-        self.rtval.set(KS.inst().rtVal('Scalar', x), KS.inst().rtVal('Scalar', y), KS.inst().rtVal('Scalar', z))
+
+
+    def equal(self, other):
+        """Checks equality of this Euler with another.
+
+        Arguments:
+        other -- Euler, other value to check equality with.
+
+        Return:
+        True if equal.
+
+        """
+
+        return self.rtval.equal('Boolean', KS.inst().rtVal('Euler', other))
+
+
+    def almostEqual(self, other, precision):
+        """Checks almost equality of this Euler with another.
+
+        Arguments:
+        other -- Euler, other value to check equality with.
+        precision -- Scalar, precision value.
+
+        Return:
+        True if almost equal.
+
+        """
+
+        return self.rtval.almostEqual('Boolean', KS.inst().rtVal('Euler', other), KS.inst().rtVal('Scalar', precision))
+
+
+    def toMat33(self):
+        """Converts the Euler angles value to a Mat33.
+
+        Return:
+        The Mat33 value
+
+        """
+
+        return Mat33(self.rtval.toMat33('Mat33'))
+
+
+
