32d676e791dd7c8515bb56c59009f5c3d13feee8
Phil Taylor <phil.taylor@fabric-engine.com>
2015-03-16 10:26:09 -0400

    Math tests can be run from within Softimage now.

--- tests/MathTests/quat.py
+++ tests/MathTests/quat.py
@@ -1,10 +1,9 @@
 from kraken.core.maths import *
 import json
 
-if __name__ == "__main__":
-    quat = Quat()
-    print "quat:" + str(quat)
-    print "mat33:" + str(quat.toMat33())
-    quat = Quat(v=Vec3(1.0, 0.0, 2.0), w=0.5);
-    print "quat:" + str(quat)
-    print "mat33:" + str(quat.toMat33())
+quat = Quat()
+print "quat:" + str(quat)
+print "mat33:" + str(quat.toMat33())
+quat = Quat(v=Vec3(1.0, 0.0, 2.0), w=0.5);
+print "quat:" + str(quat)
+print "mat33:" + str(quat.toMat33())
