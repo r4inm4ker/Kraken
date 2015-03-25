7ce8c93e8ab950b1d64ee49d18b82446f82edbc1
Eric Thivierge <ethivierge@gmail.com>
2015-03-24 21:45:32 -0400

    Created init methods for initializing config data. Override in sub-classes. Added 'get' methods for retrieving config data. Added Singleton variables and methods from Phil's example.

--- Python/kraken/core/configs/base_config.py
+++ Python/kraken/core/configs/base_config.py
@@ -9,9 +9,33 @@ BaseConfig -- Base config object used to configure builders.
 class BaseConfig(object):
     """Base Configuration for Kraken builders."""
 
+    _instance = None
+
     def __init__(self):
         super(BaseConfig, self).__init__()
-        self.explicitNaming = False
+
+        # the config is a singleton, so after the first is constructed, throw an error.
+        if BaseConfig._instance is not None:
+            raise Exception("BaseConfig object constructed twice. Please always call 'BaseConfig.getInstance'")
+
+        BaseConfig._instance = self
+
+        self._explicitNaming = self.initExplicitNaming()
+        self._colors = self.initColors()
+        self._colorMap = self.initColorMap()
+        self._nameTemplate = self.initNameTemplate()
+
+
+    # ==============
+    # Color Methods
+    # ==============
+    def initColors(self):
+        """Initializes the color values.
+
+        Return:
+        Dict, color definitions.
+
+        """
 
         # Note: These were taken from Maya and is the least common denominator since
         # you can't set colors by scalar values. :\
@@ -19,100 +43,133 @@ class BaseConfig(object):
         # Note 2: Autodesk just implemented this so we need to keep this implementation
         # for a while until it becomes the norm.
 
-        self.colors = {
-                       "black": [1, [0.00, 0.00, 0.0]],
-                       "lightGrey": [2, [0.75, 0.75, 0.75]],
-                       "darkGrey": [3, [0.50, 0.50, 0.50]],
-                       "fusia": [4, [0.80, 0.00, 0.20]],
-                       "blueDark": [5, [0.00, 0.00, 0.40]],
-                       "blue": [6, [0.00, 0.00, 1.00]],
-                       "green": [7, [0.00, 0.30, 0.00]],
-                       "purpleDark": [8, [0.20, 0.00, 0.30]],
-                       "magenta": [9, [0.80, 0.00, 0.80]],
-                       "brownLight": [10, [0.60, 0.30, 0.20]],
-                       "brownDark": [11, [0.25, 0.13, 0.13]],
-                       "orange": [12, [0.70, 0.20, 0.00]],
-                       "red": [13, [1.00, 0.00, 0.00]],
-                       "greenBright": [14, [0.00, 1.00, 0.00]],
-                       "blueMedium": [15, [0.00, 0.30, 0.60]],
-                       "white": [16, [1.00, 1.00, 1.00]],
-                       "yellow": [17, [1.00, 1.00, 0.00]],
-                       "greenBlue": [18, [0.00, 1.00, 1.00]],
-                       "turqoise": [19, [0.00, 1.00, 0.80]],
-                       "pink": [20, [1.00, 0.70, 0.70]],
-                       "peach": [21, [0.90, 0.70, 0.50]],
-                       "yellowLight": [22, [1.00, 1.00, 0.40]],
-                       "turqoiseDark": [23, [0.00, 0.70, 0.40]],
-                       "brownMuted": [24, [0.60, 0.40, 0.20]],
-                       "yellowMuted": [25, [0.63, 0.63, 0.17]],
-                       "greenMuted": [26, [0.40, 0.60, 0.20]],
-                       "turqoiseMuted": [27, [0.20, 0.63, 0.35]],
-                       "blueLightMuted": [28, [0.18, 0.63, 0.63]],
-                       "blueDarkMuted": [29, [0.18, 0.40, 0.63]],
-                       "purpleLight": [30, [0.43, 0.18, 0.63]],
-                       "mutedMagenta": [31, [0.63, 0.18, 0.40]]
-                      }
+        colors = {
+                  "black": [1, [0.00, 0.00, 0.0]],
+                  "lightGrey": [2, [0.75, 0.75, 0.75]],
+                  "darkGrey": [3, [0.50, 0.50, 0.50]],
+                  "fusia": [4, [0.80, 0.00, 0.20]],
+                  "blueDark": [5, [0.00, 0.00, 0.40]],
+                  "blue": [6, [0.00, 0.00, 1.00]],
+                  "green": [7, [0.00, 0.30, 0.00]],
+                  "purpleDark": [8, [0.20, 0.00, 0.30]],
+                  "magenta": [9, [0.80, 0.00, 0.80]],
+                  "brownLight": [10, [0.60, 0.30, 0.20]],
+                  "brownDark": [11, [0.25, 0.13, 0.13]],
+                  "orange": [12, [0.70, 0.20, 0.00]],
+                  "red": [13, [1.00, 0.00, 0.00]],
+                  "greenBright": [14, [0.00, 1.00, 0.00]],
+                  "blueMedium": [15, [0.00, 0.30, 0.60]],
+                  "white": [16, [1.00, 1.00, 1.00]],
+                  "yellow": [17, [1.00, 1.00, 0.00]],
+                  "greenBlue": [18, [0.00, 1.00, 1.00]],
+                  "turqoise": [19, [0.00, 1.00, 0.80]],
+                  "pink": [20, [1.00, 0.70, 0.70]],
+                  "peach": [21, [0.90, 0.70, 0.50]],
+                  "yellowLight": [22, [1.00, 1.00, 0.40]],
+                  "turqoiseDark": [23, [0.00, 0.70, 0.40]],
+                  "brownMuted": [24, [0.60, 0.40, 0.20]],
+                  "yellowMuted": [25, [0.63, 0.63, 0.17]],
+                  "greenMuted": [26, [0.40, 0.60, 0.20]],
+                  "turqoiseMuted": [27, [0.20, 0.63, 0.35]],
+                  "blueLightMuted": [28, [0.18, 0.63, 0.63]],
+                  "blueDarkMuted": [29, [0.18, 0.40, 0.63]],
+                  "purpleLight": [30, [0.43, 0.18, 0.63]],
+                  "mutedMagenta": [31, [0.63, 0.18, 0.40]]
+                 }
 
-        self.colorMap = {
-                         "Control": {
-                                     "default": "yellow",
-                                     "L": "greenBright",
-                                     "M": "yellow",
-                                     "R": "red"
-                                    },
-                        }
+        return colors
 
-        self.nameTemplate = {
-                             "locations": ["L", "R", "M"],
-                             "mirrorMap": {
-                                           "L": "R",
-                                           "R": "L",
-                                           "M": "M"
-                                          },
-                             "separator": "_",
-                             "types": {
-                                       "default": "null",
-                                       "Component": "cmp",
-                                       "ComponentInput": "cmpIn",
-                                       "ComponentOutput": "cmpOut",
-                                       "Container": "",
-                                       "Control": "ctrl",
-                                       "Curve": "crv",
-                                       "HierarchyGroup": "hrc",
-                                       "Joint": "def",
-                                       "Layer": "",
-                                       "Locator": "loc",
-                                       "SrtBuffer": "srtBuffer"
-                                      },
-                             "formats":
-                                       {
-                                        "default": ["component", "sep", "location", "sep", "name", "sep", "type"],
-                                        "Container": ["name"],
-                                        "Layer": ["name"],
-                                        "Component": ["name", "sep", "location", "sep", "type"]
-                                       }
-                            }
 
-    # ==============
-    # Color Methods
-    # ==============
     def getColors(self):
         """Gets the colors defined in the config.
-
-        Arguments:
-        Arguments -- Type, information.
 
         Return:
         Dict, colors.
 
         """
 
-        return self.colors
+        return self._colors
+
+
+    # ======================
+    # Color Mapping Methods
+    # ======================
+    def initColorMap(self):
+        """Initializes the color values.
+
+        Return:
+        Dict, color definitions.
+
+        """
+
+        colorMap = {
+                    "Control": {
+                                "default": "yellow",
+                                "L": "greenBright",
+                                "M": "yellow",
+                                "R": "red"
+                               }
+                   }
+
+        return colorMap
+
+
+    def getColorMap(self):
+        """Gets the color map defined in the config.
+
+        Return:
+        Dict, color map.
+
+        """
+
+        return self._colorMap
 
 
     # ======================
     # Name Template Methods
     # ======================
+    def initNameTemplate(self):
+        """Initializes the name template.
+
+        Return:
+        Dict, name template.
+
+        """
+
+        nameTemplate = {
+                        "locations": ["L", "R", "M"],
+                        "mirrorMap": {
+                                      "L": "R",
+                                      "R": "L",
+                                      "M": "M"
+                                     },
+                        "separator": "_",
+                        "types": {
+                                  "default": "null",
+                                  "Component": "cmp",
+                                  "ComponentInput": "cmpIn",
+                                  "ComponentOutput": "cmpOut",
+                                  "Container": "",
+                                  "Control": "ctrl",
+                                  "Curve": "crv",
+                                  "HierarchyGroup": "hrc",
+                                  "Joint": "def",
+                                  "Layer": "",
+                                  "Locator": "loc",
+                                  "SrtBuffer": "srtBuffer"
+                                 },
+                        "formats":
+                                  {
+                                   "default": ["component", "sep", "location", "sep", "name", "sep", "type"],
+                                   "Container": ["name"],
+                                   "Layer": ["name"],
+                                   "Component": ["name", "sep", "location", "sep", "type"]
+                                  }
+                       }
+
+        return nameTemplate
+
+
     def getNameTemplate(self):
         """Returns the naming template for this configuration.
 
@@ -121,12 +178,23 @@ class BaseConfig(object):
 
         """
 
-        return self.nameTemplate
+        return self._nameTemplate
 
 
     # =========================
     # Explicity Naming Methods
     # =========================
+    def initExplicitNaming(self):
+        """Returns the value of the explicit naming attribute.
+
+        Return:
+        Boolean, initial value for explicit naming.
+
+        """
+
+        return False
+
+
     def getExplicitNaming(self):
         """Returns the value of the explicit naming attribute.
 
@@ -135,7 +203,7 @@ class BaseConfig(object):
 
         """
 
-        return self.explicitNaming
+        return self._explicitNaming
 
 
     def setExplicitNaming(self, value):
@@ -149,24 +217,26 @@ class BaseConfig(object):
 
         """
 
-        self.explicitNaming = value
+        self._explicitNaming = value
 
         return True
 
 
-    # ===================
-    # Validation Methods
-    # ===================
-    def validateConfig(self):
-        """Validates the configuration.
+    # ==============
+    # Class Methods
+    # ==============
+    @classmethod
+    def getInstance(cls):
+        """This class method returns the singleton instance for the Config
 
         Return:
-        True if valid.
+        The singleton instance.
 
         """
 
-        # TODO: check colorMap locations are valid locations
-        # TODO: check colorMap types are valid types
-        # TODO: check colorMap type entries have a 'default' value.
+        if Config._instance is None:
+            cls()
+        elif not isinstance(Config._instance, Config):
+            raise Exception("Multiple different Config types have been constructed");
 
-        return True
+        return Config._instance
