081320b602cfbc09b819374d1eaf0799d88bc0fe
phtaylor <phil.taylor@fabricengine.com>
2015-05-15 15:49:20 -0400

    updated all unit tests

--- tests/utilityMethods/json_preprocessing.py
+++ tests/utilityMethods/json_preprocessing.py
@@ -5,8 +5,15 @@ from kraken.helpers.utility_methods import prepareToSave, prepareToLoad
 
 
 pureJSON = prepareToSave(bob_guide_data)
-print json.dumps(pureJSON, indent=2)
+str1 = json.dumps(pureJSON, indent=2)
+print str1
 
 bob_guide_data = prepareToLoad(pureJSON)
 
+# Check that we can preprocess a data struct 2X
+bob_guide_data = prepareToLoad(bob_guide_data)
 
+# Re export to pure json
+pureJSON = prepareToSave(bob_guide_data)
+str2 = json.dumps(pureJSON, indent=2)
+print str1 == str2
