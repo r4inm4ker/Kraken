19f93e7ac567cdbd903c4f56199babb98ecc9de4
Eric Thivierge <ethivierge@gmail.com>
2015-05-22 12:34:38 -0400

    Changed 'debug' parameter to 'drawDebug' to avoid name clash with default param in Softimage. Started updating components.

--- Python/kraken/plugins/si_plugin/builder.py
+++ Python/kraken/plugins/si_plugin/builder.py
@@ -324,7 +324,7 @@ class Builder(Builder):
         """
 
         parentDCCSceneItem = Dispatch(self.getDCCSceneItem(kAttribute.getParent()))
-        dccSceneItem = parentDCCSceneItem.AddParameter2(kAttribute.getName(), constants.siDouble, kAttribute.getValue(), kAttribute.getMin(), kAttribute.getMax(), kAttribute.getMin(), kAttribute.getMax(), constants.siClassifUnknown, 2053, kAttribute.getName())
+        dccSceneItem = parentDCCSceneItem.AddParameter2(kAttribute.getName(), constants.siDouble, kAttribute.getValue(), kAttribute.getMin(), kAttribute.getMax(), kAttribute.getUIMin(), kAttribute.getUIMax(), constants.siClassifUnknown, 2053, kAttribute.getName())
 
         self._registerSceneItemPair(kAttribute, dccSceneItem)
 
@@ -343,7 +343,7 @@ class Builder(Builder):
         """
 
         parentDCCSceneItem = Dispatch(self.getDCCSceneItem(kAttribute.getParent()))
-        dccSceneItem = parentDCCSceneItem.AddParameter2(kAttribute.getName(), constants.siInt4, kAttribute.getValue(), kAttribute.min, kAttribute.max, kAttribute.min, kAttribute.max, constants.siClassifUnknown, 2053, kAttribute.getName())
+        dccSceneItem = parentDCCSceneItem.AddParameter2(kAttribute.getName(), constants.siInt4, kAttribute.getValue(), kAttribute.getMin(), kAttribute.getMax(), kAttribute.getUIMin(), kAttribute.getUIMax(), constants.siClassifUnknown, 2053, kAttribute.getName())
 
         self._registerSceneItemPair(kAttribute, dccSceneItem)
 
