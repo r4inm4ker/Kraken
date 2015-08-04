48c8c780db28fbf338f8a111599e9aeecd5a3171
phtaylor <phil.taylor@fabricengine.com>
2015-08-04 12:28:36 -0400

    updated all tests. fixed minor regressions

--- tests/ComponentTests/tentacle_build.py
+++ tests/ComponentTests/tentacle_build.py
@@ -1,24 +1,4 @@
 
-import os
-import sys
-
-
-krakenDir = os.path.dirname(os.path.realpath(__file__))
-# krakenDir=os.path.abspath(os.path.join(krakenModuleDir, '..', '..'))
-
-fabricEngineDir=os.path.normpath("D:/temp/FabricEngine-1.15.2-Windows-x86_64/")
-
-os.environ['PATH'] = os.path.join(fabricEngineDir, 'bin') + ';' + os.environ['PATH']
-
-PYTHON_VERSION = sys.version[:3]
-sys.path.append( os.path.join(fabricEngineDir, 'Python', PYTHON_VERSION ) )
-
-os.environ['FABRIC_EXTS_PATH'] = os.path.join(fabricEngineDir, 'Exts') + ';' + os.path.join(krakenDir, 'KLExts') + ';' + os.environ['FABRIC_EXTS_PATH']
-
-os.environ['KRAKEN_PATHS'] = os.path.join(krakenDir, 'extraComponents')
-
-
-
 from kraken import plugins
 from kraken.core.maths import Vec3
 from kraken_examples.tentacle_component import TentacleComponentGuide, TentacleComponentRig
@@ -34,13 +14,20 @@ tentacleGuide.loadData(
                         {
                          "name": "tentacle",
                          "location": "L",
-                         "numJoints": 4,
+                         "numJoints": 12,
                          "jointPositions": [
-                                            Vec3(0.9811, 9.769, -1.237),
-                                            Vec3(5.4488, 8.4418, -1.237),
-                                            Vec3(4.0, 3.1516, -1.237),
-                                            Vec3(6.841, 1.0, -1.237),
-                                            Vec3(9.841, 0.0, -1.237)
+                                            Vec3(0.9811,    12, -1.237),
+                                            Vec3(5.4488,    11, -1.237),
+                                            Vec3(4.0,       10, -1.237),
+                                            Vec3(6.841,     9, -1.237),
+                                            Vec3(9.841,     8, -1.237),
+                                            Vec3(9.841,     7, -1.237),
+                                            Vec3(9.841,     6, -1.237),
+                                            Vec3(9.841,     5, -1.237),
+                                            Vec3(9.841,     4, -1.237),
+                                            Vec3(9.841,     3, -1.237),
+                                            Vec3(9.841,     2, -1.237),
+                                            Vec3(9.841,     1, -1.237)
                                            ]
                         })
 
