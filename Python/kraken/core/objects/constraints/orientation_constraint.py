6d0702b84a1fb1ad820dd66f56e7516ba852d49e
Eric Thivierge <ethivierge@gmail.com>
2015-09-16 17:16:42 -0400

    Updated constraints and operator doc strings.

--- Python/kraken/core/objects/constraints/orientation_constraint.py
+++ Python/kraken/core/objects/constraints/orientation_constraint.py
@@ -20,8 +20,8 @@ class OrientationConstraint(Constraint):
     def evaluate(self):
         """invokes the constraint causing the output value to be computed.
 
-        Return:
-        Boolean, True if successful.
+        Returns:
+            bool: True if successful.
 
         """
 
--- Python/kraken/core/objects/operators/splice_operator.py
+++ Python/kraken/core/objects/operators/splice_operator.py
@@ -54,8 +54,8 @@ class SpliceOperator(Operator):
     def getSolverTypeName(self):
         """Returns the solver type name for this operator.
 
-        Return:
-        String, name of the solver type this operator uses.
+        Returns:
+            str: Name of the solver type this operator uses.
 
         """
 
@@ -65,8 +65,8 @@ class SpliceOperator(Operator):
     def getExtension(self):
         """Returns the extention this operator uses.
 
-        Return:
-        String, name of the extension this solver uses.
+        Returns:
+            str: Name of the extension this solver uses.
 
         """
 
@@ -88,8 +88,8 @@ class SpliceOperator(Operator):
     def getSolverArgs(self):
         """Returns the args array defined by the KL Operator.
 
-        Return:
-        RTValArray, args array defined by the KL Operator.
+        Returns:
+            RTValArray: Args array defined by the KL Operator.
 
         """
 
@@ -99,8 +99,8 @@ class SpliceOperator(Operator):
     def generateSourceCode(self, arraySizes={}):
         """Returns the source code for a stub operator that will invoke the KL operator
 
-        Return:
-        String, The source code for the stub operator.
+        Returns:
+            str: The source code for the stub operator.
 
         """
 
@@ -160,8 +160,8 @@ class SpliceOperator(Operator):
     def evaluate(self):
         """invokes the Splice operator causing the output values to be computed.
 
-        Return:
-        Boolean, True if successful.
+        Returns:
+            bool: True if successful.
 
         """
 
