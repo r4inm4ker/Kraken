93301c85a49dc2115f766495e605920174019f35
phtaylor <phil@hordesoftware.co>
2015-10-23 15:51:15 -0400

    implemented CanvasOperator

---
+++ Python/kraken/core/objects/operators/canvas_operator.py
@@ -0,0 +1,172 @@
+"""Kraken - objects.operators.kl_operator module.
+
+Classes:
+CanvasOperator - Splice operator object.
+
+"""
+
+from kraken.core.maths import Mat44
+from kraken.core.objects.object_3d import Object3D
+from kraken.core.objects.operators.operator import Operator
+from kraken.core.objects.attributes.attribute import Attribute
+from kraken.core.kraken_system import ks
+
+
+class CanvasOperator(Operator):
+    """Splice Operator representation."""
+
+    # TODO: Look in to expanding the Splice operator to be able to handle more
+    # than one extension / operator. Need to change extension to extensions and
+    # figure out how to differentiate the solver types per operator. Maybe have
+    # an attirbute array called 'klOperators' that contains sets of what we
+    # currently have setup.
+
+    def __init__(self, name, canvasPresetName):
+        super(CanvasOperator, self).__init__(name)
+
+        self.canvasPresetName = canvasPresetName
+
+        # Load the Fabric Engine client and construct the RTVal for the Solver
+        host = ks.getCoreClient().DFG.host
+
+
+        def getPresetDesc(path):
+            fileContents = open( host.getPresetImportPathname(path) ).read()
+            fileContents = "".join(fileContents.split('\n'))
+            fileContents = "  ".join(fileContents.split('\t'))
+            return json.loads(fileContents)
+        self.graphDesc = getPresetDesc(self.canvasGraphName)
+        self.args = self.solverRTVal.getArguments('KrakenSolverArg[]')
+
+        # Initialize the inputs and outputs based on the given args.
+        for port in self.graphDesc['ports']:
+            portName = port['name']
+            portConnectionType = port['execPortType']
+            if 'typeSpec' in port:
+                portDataType = port['typeSpec']
+            else:
+                portDataType = '$TYPE$'
+
+            if portConnectionType == 'In':
+                if portDataType.endswith('[]'):
+                    self.inputs[portName] = []
+                else:
+                    self.inputs[portName] = None
+            else:
+                if portDataType.endswith('[]'):
+                    self.outputs[portName] = []
+                else:
+                    self.outputs[portName] = None
+
+
+    def getPresetTypeName(self):
+        """Returns the solver type name for this operator.
+
+        Returns:
+            str: Name of the solver type this operator uses.
+
+        """
+
+        return self.canvasGraphName
+
+
+    def getExtension(self):
+        """Returns the extention this operator uses.
+
+        Returns:
+            str: Name of the extension this solver uses.
+
+        """
+
+        return self.extension
+
+
+    def getSolverArgs(self):
+        """Returns the args array defined by the KL Operator.
+
+        Returns:
+            RTValArray: Args array defined by the KL Operator.
+
+        """
+
+        return self.args
+
+
+    def evaluate(self):
+        """invokes the Splice operator causing the output values to be computed.
+
+        Returns:
+            bool: True if successful.
+
+        """
+
+        def getRTVal(obj):
+            if isinstance(obj, Object3D):
+                return obj.xfo.getRTVal().toMat44('Mat44')
+            elif isinstance(obj, Attribute):
+                return obj.getRTVal()
+
+        argVals = []
+        for port in self.graphDesc['ports']:
+            portName = port['name']
+            portConnectionType = port['execPortType']
+            portDataType = port['typeSpec']
+
+            if portDataType == '$TYPE$':
+                return
+
+            if portDataType == 'EvalContext':
+                argVals.append(ks.constructRTVal(portDataType))
+                continue
+            if argName == 'time':
+                argVals.append(ks.constructRTVal(portDataType))
+                continue
+            if argName == 'frame':
+                argVals.append(ks.constructRTVal(portDataType))
+                continue
+
+            if portConnectionType == 'in':
+                if str(portDataType).endswith('[]'):
+                    rtValArray = ks.rtVal(portDataType[:-2]+'Array')
+                    rtValArray.resize(len(self.inputs[argName]))
+                    for j in xrange(len(self.inputs[argName])):
+                        rtValArray[j] = getRTVal(self.inputs[argName][j])
+                    argVals.append(rtValArray)
+                else:
+                    argVals.append(getRTVal(self.inputs[argName]))
+            else:
+                if str(portDataType).endswith('[]'):
+                    rtValArray = ks.rtVal(portDataType[:-2]+'Array')
+                    rtValArray.resize(len(self.outputs[argName]))
+                    for j in xrange(len(self.outputs[argName])):
+                        rtValArray[j] = getRTVal(self.outputs[argName][j])
+                    argVals.append(rtValArray)
+                else:
+                    argVals.append(getRTVal(self.outputs[argName]))
+
+
+        host = ks.getCoreClient().DFG.host
+        binding = host.createBindingToPreset(self.canvasPresetName, argVals)
+        binding.execute()
+
+        # Now put the computed values out to the connected output objects.
+        def setRTVal(obj, rtval):
+            if isinstance(obj, Object3D):
+                obj.xfo.setFromMat44(Mat44(rtval))
+            elif isinstance(obj, Attribute):
+                obj.setValue(rtval)
+
+        for port in self.graphDesc['ports']:
+            portName = port['name']
+            portConnectionType = port['execPortType']
+            portDataType = '$TYPE$'
+
+            if portConnectionType != 'In':
+                outVal = binding.getArgValue(portName)
+                if portDataType.endswith('[]'):
+                    for j in xrange(len(outVal)):
+                        setRTVal(self.outputs[argName][j], outVal[j])
+                else:
+                    setRTVal(self.outputs[argName], outVal)
+
+        return True
