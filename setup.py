eceb53ed98cc81df1f4ee1784b431f48eca3c29e
Eric Thivierge <ethivierge@gmail.com>
2016-01-14 16:03:55 -0500

    Added setup.py so users will be able to install using pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Kraken',
+      version='1.1.0',
+      description='Kraken Rigging Framework',
+      author='Eric Thivierge, Phil Taylor',
+      author_email='ethivierge@gmail.com',
+      url='http://fabric-engine.github.io/Kraken//',
+      packages=['kraken']
+     )
