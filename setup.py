2adbcfffbf72883741ebbd6719dac56c50681989
Eric Thivierge <ethivierge@gmail.com>
2016-01-14 17:17:53 -0500

    Updated setup.py with more info and fixes.

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from distutils.core import setup
 
+
+# Requirements.
+setup_requires = [
+    'PySide == 1.2.2'
+    ]
+
+install_requires = []
+
+
 setup(name='Kraken',
       version='1.1.0',
       description='Kraken Rigging Framework',
       author='Eric Thivierge, Phil Taylor',
       author_email='ethivierge@gmail.com',
       url='http://fabric-engine.github.io/Kraken//',
-      package_dir = {'': 'Python'}
+      license='BSD 3-clause "New" or "Revised" License',
+      packages = ['kraken'],
+      package_dir = {'': 'Python'},
+      setup_requires = setup_requires,
+      install_requires = install_requires,
      )
