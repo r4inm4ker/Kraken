810385998faca9f48eea98f6d5b80ed7415f053f
Eric Thivierge <ethivierge@gmail.com>
2016-01-14 15:05:46 -0500

    Add example launcher scripts.

---
+++ extras/launcher_scripts/windows/Maya 2016 Kraken_Fabric2.0.bat
@@ -0,0 +1,11 @@
+call D:\fabric\FabricEngine-2.0.1-Windows-x86_64\environment.bat
+
+
+set KRAKEN_PATH=D:\dev\kraken
+set FABRIC_EXTS_PATH=%FABRIC_EXTS_PATH%;%KRAKEN_PATH%\Exts;
+set FABRIC_DFG_PATH=%FABRIC_DFG_PATH%;%KRAKEN_PATH%\Presets\DFG;
+set PYTHONPATH=%PYTHONPATH%;%KRAKEN_PATH%\Python;
+
+set MAYA_MODULE_PATH=%MAYA_MODULE_PATH%;%FABRIC_DIR%\DCCIntegrations\FabricMaya2016;%KRAKEN_PATH%\DCCIntegrations\maya;
+
+start /d "C:\Program Files\Autodesk\Maya2016\bin" maya.exe
