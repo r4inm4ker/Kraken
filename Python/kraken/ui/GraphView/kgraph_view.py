c8c7bc2e3b60b5d40dc6894953f39a5d88ea34ae
Eric Thivierge <ethivierge@gmail.com>
2016-03-07 18:55:36 -0500

    We don't clear selection on left click in the graph on mousePressEvent. We now take care of this in the mouseReleaseEvent.

--- Python/kraken/ui/GraphView/kgraph_view.py
+++ Python/kraken/ui/GraphView/kgraph_view.py
@@ -191,7 +191,6 @@ class KGraphView(GraphView):
             self.beginNodeSelection.emit()
             self._manipulationMode = 1
             self._mouseDownSelection = copy.copy(self.getSelectedNodes())
-            self.clearSelection(emitSignal=False)
             self._selectionRect = SelectionRect(graph=self, mouseDownPos=self.mapToScene(event.pos()))
 
         elif event.button() is QtCore.Qt.MouseButton.MiddleButton:
@@ -235,6 +234,8 @@ class KGraphView(GraphView):
             component.setGraphPos(Vec2(dropPosition.x(), dropPosition.y()))
             node = KNode(self, component)
             self.addNode(node)
+
+            self.selectNode(node, clearSelection=True, emitSignal=False)
 
             event.acceptProposedAction()
         else:
