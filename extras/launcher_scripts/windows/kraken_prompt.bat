3530dfea967d73aec93ec7aeca5c9aebfcc56f11
Eric Thivierge <ethivierge@gmail.com>
2016-04-25 13:09:01 -0400

    Updated sample launcher scripts and renamed them.

--- extras/launcher_scripts/windows/kraken_prompt.bat
+++ extras/launcher_scripts/windows/kraken_prompt.bat
@@ -1,11 +1,13 @@
-call D:\fabric\FabricEngine-2.0.1-Windows-x86_64\environment.bat
+@ECHO OFF
+ECHO "Releasing The Kraken!"
 
+call D:\fabric\FabricEngine-2.1.0-Windows-x86_64\environment.bat
 
 set KRAKEN_PATH=D:\dev\kraken
 set FABRIC_EXTS_PATH=%FABRIC_EXTS_PATH%;%KRAKEN_PATH%\Exts;
 set FABRIC_DFG_PATH=%FABRIC_DFG_PATH%;%KRAKEN_PATH%\Presets\DFG;
 set PYTHONPATH=%PYTHONPATH%;%KRAKEN_PATH%\Python;
 
-call "C:\Program Files\Autodesk\Softimage 2015 SP1\Application\bin\XSI.bat"
+cd /d %KRAKEN_PATH%
 
-echo OFF
+call cmd
