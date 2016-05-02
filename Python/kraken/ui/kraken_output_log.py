90e5969d796fe9d02c71496e7af7d6c72641bbe7
Eric Thivierge <ethivierge@gmail.com>
2016-05-02 15:46:36 -0400

    Created separate modules for output log and splash screen.
    Widgets automatically add themselves to the logger's widget handler.

---
+++ Python/kraken/ui/kraken_output_log.py
@@ -0,0 +1,89 @@
+
+from PySide import QtGui, QtCore
+
+from kraken.ui import images_rc
+from kraken.log import getLogger
+
+logger = getLogger('kraken')
+
+
+class OutputLogDialog(QtGui.QDialog):
+    """Output Dialog"""
+
+    def __init__(self, parent=None):
+        super(OutputLogDialog, self).__init__(parent)
+        self.setObjectName('outputLog')
+        self.resize(700, 300)
+        self.setWindowTitle('Kraken Output Log')
+
+        for handler in logger.handlers:
+            if type(handler).__name__ == 'WidgetHandler':
+                handler.addWidget(self)
+
+        self.createLayout()
+        self.createConnections()
+
+
+    def createLayout(self):
+        """Sets up the layout for the dialog."""
+
+        self.textWidget = QtGui.QTextEdit()
+        self.textWidget.setLineWrapMode(QtGui.QTextEdit.NoWrap)
+        self.textWidget.setReadOnly(True)
+        self.textWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+
+        self.outputLogLayout = QtGui.QVBoxLayout(self)
+        self.outputLogLayout.addWidget(self.textWidget)
+
+        self.setLayout(self.outputLogLayout)
+
+    def createConnections(self):
+        """Connects widgets to methods or other signals."""
+
+        self.textWidget.customContextMenuRequested.connect(self.createContextMenu)
+
+    def write(self, msg, level):
+
+        if level == 'DEBUG':
+            messageColor = QtGui.QColor("#B4EEB4")
+        elif level == 'INFO':
+            messageColor = QtGui.QColor(QtCore.Qt.white)
+        elif level == 'WARNING':
+            messageColor = QtGui.QColor("#CC3300")
+        elif level == 'ERROR':
+            messageColor = QtGui.QColor("#FF0000")
+        elif level == 'CRITICAL':
+            messageColor = QtGui.QColor("#FF0000")
+        else:
+            messageColor = QtGui.QColor(QtCore.Qt.white)
+
+        self.textWidget.setTextColor(messageColor)
+        charFormat = self.textWidget.currentCharFormat()
+        textCursor = self.textWidget.textCursor()
+        textCursor.movePosition(QtGui.QTextCursor.End)
+        textCursor.insertText(msg, charFormat)
+
+        self.textWidget.setTextCursor(textCursor)
+        self.textWidget.ensureCursorVisible()
+
+    # =============
+    # Context Menu
+    # =============
+    def createContextMenu(self):
+        self.contextMenu = QtGui.QMenu(self)
+        selectAllAction = self.contextMenu.addAction("Select All")
+        copyAction = self.contextMenu.addAction("Copy")
+        self.contextMenu.addSeparator()
+        clearAction = self.contextMenu.addAction("Clear")
+
+        selectAllAction.triggered.connect(self.contextSelectAll)
+        copyAction.triggered.connect(self.contextCopy)
+        clearAction.triggered.connect(self.textWidget.clear)
+
+        self.contextMenu.exec_(QtGui.QCursor.pos())
+
+    def contextSelectAll(self):
+        self.textWidget.selectAll()
+
+    def contextCopy(self):
+        self.textWidget.copy()
---
+++ Python/kraken/ui/kraken_splash.py
@@ -0,0 +1,46 @@
+import re
+import sys
+
+from PySide import QtGui, QtCore
+
+from kraken.ui import images_rc
+from kraken.log import getLogger
+
+logger = getLogger('kraken')
+
+
+class KrakenSplash(QtGui.QSplashScreen):
+
+    messageFlag = QtCore.Qt.AlignBottom | QtCore.Qt.AlignLeft
+
+    def __init__(self, app, *arg, **kwargs):
+
+        splashPixmap = QtGui.QPixmap(':/images/KrakenUI_Splash.png')
+        super(KrakenSplash, self).__init__(splashPixmap)
+
+        for handler in logger.handlers:
+            if type(handler).__name__ == 'WidgetHandler':
+                handler.addWidget(self)
+
+        self._msg = []
+        self._app = app
+        self.setMask(splashPixmap.mask())
+        self.showMessage('Releasing the Kraken!', self.messageFlag, QtCore.Qt.white)
+        self._app.processEvents()
+
+    # def write(self, msg):
+    #     filtered = filter(lambda x: not re.match(r'^$', x), msg)
+
+    #     if filtered != "":
+    #         if len(self._msg) == 1:
+    #             del self._msg[0]
+
+    #         self._msg.append(filtered)
+
+    #         self.showMessage("\n".join(self._msg), self.messageFlag, QtCore.Qt.white)
+
+    def write(self, msg, level):
+        filtered = filter(lambda x: not re.match(r'^$', x), msg)
+
+        if filtered != "":
+            self.showMessage(msg.rstrip(), self.messageFlag, QtCore.Qt.white)
