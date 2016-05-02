5151d6fd623fab2ddadf5c992dfe5a01a4a95e06
Eric Thivierge <ethivierge@gmail.com>
2016-05-02 12:57:09 -0400

    Added widget handler and new custom status bar widget.

---
+++ Python/kraken/log/widget_handler.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class WidgetHandler(logging.Handler):
+    """Logging Handler for Sending Messages to Widgets."""
+
+    def __init__(self):
+        super(WidgetHandler, self).__init__()
+        self._widgets = []
+
+    def addWidget(self, widget):
+        if widget not in self._widgets:
+            self._widgets.append(widget)
+
+    def removeWidet(self, widget):
+        if widget in self._widgets:
+            self._widgets.remove(widget)
+
+    def emit(self, record):
+
+        msg = self.format(record)
+
+        for widget in self._widgets:
+            widget.write(msg + '\n', record.levelname)
---
+++ Python/kraken/ui/kraken_statusbar.py
@@ -0,0 +1,76 @@
+from PySide import QtGui, QtCore
+
+
+class KrakenStatusBar(QtGui.QStatusBar):
+    """Custom status bar widget for Kraken."""
+
+    def __init__(self, parent):
+        super(KrakenStatusBar, self).__init__(parent)
+
+    def showMessage(self, message, timeout=0):
+        super(KrakenStatusBar, self).showMessage(message, timeout)
+
+    def write(self, msg, level):
+
+        messageConfig = {
+            'DEBUG': {
+                'color': '#B4EEB4',
+                'timeout': 3500
+            },
+            'INFO': {
+                'color': '#FFFFFF',
+                'timeout': 3500
+            },
+            'WARNING': {
+                'color': '#D89614',
+                'timeout': 3500
+            },
+            'ERROR': {
+                'color': '#CC0000',
+                'timeout': 0
+            },
+            'CRITICAL': {
+                'color': '#CC0000',
+                'timeout': 0
+            }
+        }
+
+        timeOut = messageConfig[level]['timeout']
+
+        # Remove current labels
+        currentLabels = self.findChildren(QtGui.QLabel)
+        for label in currentLabels:
+            self.removeWidget(label)
+
+        if level == "INFO":
+            self.showMessage(msg, timeOut)
+        else:
+            lines = msg.split("\n")
+            if len(lines) > 0:
+                msg = lines[0][:120]
+            else:
+                msg = msg[:120]
+
+            messageLabel = QtGui.QLabel(msg)
+            messageLabel.setStyleSheet("QLabel { border-radius: 3px; background-color: " + messageConfig[level]['color'] + "}")
+
+            def addMessage():
+                self.clearMessage()
+                self.currentMessage = messageLabel
+                self.addWidget(messageLabel, 1)
+                self.repaint()
+                if timeOut > 0.0:
+                    timer.start()
+
+            def endMessage():
+                timer.stop()
+                self.removeWidget(messageLabel)
+                self.repaint()
+                self.showMessage('Ready', 2000)
+
+            if timeOut > 0.0:
+                timer = QtCore.QTimer()
+                timer.setInterval(timeOut)
+                timer.timeout.connect(endMessage)
+
+            addMessage()
